024c48b469cacdcc5b45b3fd1c3dc1ec11157c11
Moritz <moritz.j.bauer@gmail.com>
2025-04-04 20:19:15 +0200

    Update gtm_botd_templ.js

--- gtm_botd_templ.js
+++ gtm_botd_templ.js
@@ -3,7 +3,7 @@
     const Botd = await import('https://openfpcdn.io/botd/v1');
     const botd = await Botd.load();
     const result = await botd.detect();
-    console.log(result);
+    window.gtm_is_bot = result.bot;
 
   } catch (error) {
     console.error(error);
